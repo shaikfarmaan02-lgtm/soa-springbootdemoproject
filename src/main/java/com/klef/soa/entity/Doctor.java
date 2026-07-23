7ff972c7026bfcc228816dfe929386ff10f9cd52
Shaik Farmaan <shaikfarmaan02@gmail.com>
2026-07-23 17:18:55 +0530

    Project v1.0

---
+++ src/main/java/com/klef/soa/entity/Doctor.java
@@ -0,0 +1,129 @@
+package com.klef.soa.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="doctor_table")
+public class Doctor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "did")
+	private long id;
+	@Column(name="dname", length=100,nullable=false)
+	private String name;
+	@Column(name = "dgender", length=20,nullable=false)
+	private String gender;
+	@Column(nullable = false)
+	private String specialization;
+	@Column(name = "dsalary",nullable=false)
+		private Double salary;
+	@Column(name="dname", length=100, nullable=false, unique=true)
+	private String email;
+	@Column(length=20,nullable=false,unique=true)
+	private String contact;
+	@Column(nullable=false)
+	private String location;
+	@Column(name = "dstatus",nullable=false)
+	private Boolean status;
+	@Column(nullable=false)
+	private Double expereince;
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getSpecialization() {
+		return specialization;
+	}
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setAge(Double salary) {
+		this.salary = salary;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	public Double getExpereince() {
+		return expereince;
+	}
+	public void setExpereince(Double expereince) {
+		this.expereince = expereince;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Doctor [id=" + id + ", name=" + name + ", gender=" + gender + ", specialization=" + specialization
+				+ ", salary=" + salary + ", email=" + email + ", contact=" + contact + ", location=" + location
+				+ ", status=" + status + ", expereince=" + expereince + "]";
+	}
+
+	
+
+}
